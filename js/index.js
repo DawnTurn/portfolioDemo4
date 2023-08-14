b3300d4ac79c3b238752557b6f1246bc10d176fa
timmy turner <adebowaleademuyiwa12@gmail.com>
2023-08-15 00:48:42 +0100

    update desktop view

--- js/index.js
+++ js/index.js
@@ -2,11 +2,19 @@ const burger = document.querySelector('.burger')
 const body = document.querySelector('body')
 const tl = gsap.timeline()
 
-tl.to('nav', .5, {opacity: 1, pointerEvents: 'all'})
-  .to('nav ul a', 1, {y: 0, ease: Power2.easeInOut}, '-=.3')
-  .fromTo('nav a i', .5, {opacity: 0, y: '10px'}, {opacity: 1, y: 0, ease: Power4.easeInOut, stagger: .07}, '-=.4')
-  .fromTo('.socials h3', .3, {opacity: 0}, {opacity: 1}, '-=.6')
-  .reverse()
+if (window.matchMedia("(max-width: 1024px)").matches){
+  tl.to("nav", 0.5, { opacity: 1, pointerEvents: "all" })
+    .to("nav ul a", 1, { y: 0, ease: Power2.easeInOut }, "-=.3")
+    .fromTo(
+      "nav a i",
+      0.5,
+      { opacity: 0, y: "10px" },
+      { opacity: 1, y: 0, ease: Power4.easeInOut, stagger: 0.07 },
+      "-=.4"
+    )
+    .fromTo(".socials h3", 0.3, { opacity: 0 }, { opacity: 1 }, "-=.6")
+    .reverse();
+}
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active')
